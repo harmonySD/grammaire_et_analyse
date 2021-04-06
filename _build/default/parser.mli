e2e512f590bac4121445479dab6cebdaab7f5253
Clément Blérald <clement.blerald@gmail.com>
2021-04-06 18:13:44 +0200

    corrige

---
+++ _build/default/parser.mli
@@ -0,0 +1,27 @@
+
+(* The type of tokens. *)
+
+type token = 
+  | VAR
+  | TOURNE
+  | RPAR
+  | PLUS
+  | NB of (int)
+  | MOINS
+  | LPAR
+  | IDENT of (string)
+  | HPINCEAU
+  | FIN
+  | EOF
+  | EGAL
+  | DEB
+  | BPINCEAU
+  | AVANCE
+
+(* This exception is raised by the monolithic API functions. *)
+
+exception Error
+
+(* The monolithic API. *)
+
+val programme: (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (Ast.programme)
