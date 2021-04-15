f8c2b1d874d8b2cbca94ccaa7365f4ab0e5b9479
Clément Blérald <clement.blerald@gmail.com>
2021-04-15 15:16:51 +0200

    Normalement ok

--- interpreter.ml
+++ interpreter.ml
@@ -3,21 +3,39 @@ open Float
 
 exception Quit;;
 
-let angle  = ref 0;;
+(* let angle = ref 0;; *)
 let pinceau = ref false;;
 
-(* calculate next pos *)
-let nextPos (v : int) =
-  let (currX,currY) = current_point () in
-  let pi = 3.1415926535 in
+(*GERER BUG COORDONNE*)
+(*ENLEVER PINCEAU*)
+(*GERER LES DEUX ERREURS DIV 0 ET SORTIE CANVAS*)
 
-  let a = (of_int currX) +. (of_int v) *. cos (of_int !angle *. (pi /. 180.)) 
-  and b = (of_int currY) +. (of_int v) *. sin (of_int !angle *. (pi /. 180.)) in
+
+(*FONCTIONS AUX *)
+let modulo x y =
+  let a = x mod y in
+  if a < 0 then a + y    
+  else a
+
+let rec myprint (l:(string * int) list) =
+    match l with
+    | [] -> ""
+    | x::ll -> 
+      let (a,b) = x in
+      "('"^a^"',"^(string_of_int b)^")" ^ myprint ll
+        
+let nextPos (v : int) (env : (string * int) list) =
+  let (currX,currY) = current_point ()
+  and pi = 3.1415926535 
+  and rot = modulo ((List.assoc "rot" env) + 90) 360 in
+
+  let a = (of_int currX) +. (of_int v) *. cos (of_int rot *. (pi /. 180.)) 
+  and b = (of_int currY) +. (of_int v) *. sin (of_int rot *. (pi /. 180.)) in
   
-  let fa = if a > (floor a) +. 0.5 then ceil a else floor a
-  and fb = if b > (floor b) +. 0.5 then ceil b else floor b in
+  let fa = abs (if a > (floor a) +. 0.5 then ceil a else floor a)
+  and fb = abs (if b > (floor b) +. 0.5 then ceil b else floor b) in
 
-  (to_int fa, to_int fb)
+  (to_int fa,to_int fb)
 
 let rec get_values (expr : Ast.expression) (env : (string * int) list): int =
   match expr with
@@ -26,52 +44,71 @@ let rec get_values (expr : Ast.expression) (env : (string * int) list): int =
   | Moins (e1,e2) -> (get_values e1 env) - (get_values e2 env)
   | Nombre i -> i 
 
+(*FIN FONCTIONS AUX*)
+
 let draw (env : (string * int) list) (instruct : Ast.instruction) : (string * int) list =
   match instruct with
   | Avance e ->
-    let tmp = get_values e env in
-    let (x,y) = nextPos tmp in
-    lineto x y;
-    env 
-  | Tourne e -> angle := !angle + (get_values e env); env
-  | Egal (s,e) -> (s,(get_values e env))::env
-  | BasPinceau -> pinceau := true; env
-  | HautPinceau -> pinceau := false; env
+    if !pinceau = true then 
+      begin 
+        print_string "Avance\n";
+        let tmp = get_values e env in
+        let (x,y) = nextPos tmp env in
+        let (currx, curry) = current_point() in
+        print_string "env : ";
+        print_string (myprint env);
+        print_newline();
+        print_string "rot : ";
+        print_int (List.assoc "rot" env);
+        print_newline();
 
-let rec loop()= loop()
+        print_string "x : ";
+        print_int currx;
+        print_newline();
+        print_string "y : ";
+        print_int curry;
+        print_newline();
+
+        print_string "nouveau x : ";
+        print_int x;
+        print_newline();
+
+        print_string "nouveau y : ";
+        print_int y;
+        print_newline();
+        print_newline();
+        print_newline();
+        lineto x y;
+        env
+      end
+    else 
+      env
+  | Tourne e ->
+    print_string "tourne";
+    print_newline();
+    let rot = List.assoc "rot" env 
+    and angle = List.assoc "angle" env in
+    print_string "e : ";
+    print_int (get_values e env);
+    print_newline();
+    print_newline();
+    ("rot",(modulo (rot + angle) 360))::(List.remove_assoc "rot" (List.rev env))
+  | Egal (s,e) ->
+    print_string "Egal";
+    print_newline();
+    print_string (myprint env);
+    print_newline();
+    print_newline();
+    (s,(get_values e env))::env
+  | BasPinceau -> pinceau := true; print_string "BasPinceau"; print_newline();env
+  | HautPinceau -> pinceau := false; print_string "HautPinceau"; print_newline();env
 
 let init (ast : Ast.programme) : unit =
-  let _ = Printf.printf "Parse:\n%s\n" (Ast.as_string_programme ast) in
   let (_,instruct) = ast in
   open_graph " 500x500";
 
-  print_int (List.length instruct);
-  print_string "on est là\n";
-  print_newline();
+  moveto 100 100;
+  ignore (List.fold_left draw [("rot",0)] instruct);
 
-  let nextLine (instruct : Ast.instruction list) =  
-
-    print_string "cas 1";
-    print_newline();
-    ignore(List.fold_left (draw) ([]) (instruct))
-  in 
-
-  print_string "cas 0";
-  print_newline();
-  try nextLine instruct
-  with _ -> close_graph()
- (*
-  let rec boucle (ax : symbol word) =
-    let ev = wait_next_event [Key_pressed] in
-    if ev.key == 'q' then raise Quit;
-    begin
-      clear_graph();
-      let cmd = getCommands obj.interp ax in
-      display_all cmd pile;
-      let nextStr = nextAxiom obj.rules ax in
-      boucle nextStr
-    end
-  in
-  let chaine = obj.axiom in
-  try boucle chaine with Quit -> close_graph();
-*)
+  let ev = wait_next_event [Key_pressed] in
+  if ev.keypressed then close_graph()
