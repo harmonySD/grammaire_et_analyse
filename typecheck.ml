cf5315755cb1e837cf6387d89a0e349cf4d0ae7f
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2021-04-25 21:06:29 +0200

    erreur de typo

--- typecheck.ml
+++ typecheck.ml
@@ -6,7 +6,7 @@ let rec get_declarations = function
   | [] -> []
   | v::r -> let rl = get_declarations r in 
                 if List.mem v rl
-                then raise (Error("Declarer deux fois : "^v))
+                then raise (Error("Declarée deux fois : "^v))
                 else v::rl
 
 let rec type_expression decs = function
@@ -28,18 +28,18 @@ let rec check_instruction decs = function
     try 
       if List.mem s decs && (type_expression decs e) = 1
       then ()
-      else raise (Error ("Pas declarer "^s))
+      else raise (Error ("Pas declarée "^s))
     with Not_found ->
-      raise (Error ("Pas declarer "^s))
+      raise (Error ("Pas declarée "^s))
     )
   | Avance e -> 
     if (type_expression decs e) = 1 
     then ()
-    else raise (Error ("Avance argument n'est pas une expression reconnue")) 
+    else raise (Error ("Avance : argument n'est pas une expression reconnue")) 
   | Tourne e ->
     if (type_expression decs e) = 1 
     then ()
-    else raise (Error ("Tourne argument n'est pas une expression reconnue"))
+    else raise (Error ("Tourne : argument n'est pas une expression reconnue"))
   | BasPinceau -> ()
   | HautPinceau -> ()
   | Sias(e,i1,i2) ->
@@ -64,14 +64,14 @@ let rec check_instruction decs = function
     if r >= 256 || r < 0 || g >= 256 && g < 0 || b >= 256 || b < 0 
     then raise (Error("Erreur de type pour les couleurs"))
     else ()
-  | Epaisseur t -> if t < 0 then raise(Error("Erreur de type pour l'epaisseur"))
+  | Epaisseur t -> if t < 0 then raise(Error("Erreur de type pour l'épaisseur"))
               
 and check_instructions decs il =
   List.iter
     (function i -> check_instruction decs i)
     il
 
-(* Verificationn des types d'un programme complet *)
+(* Verification des types d'un programme complet *)
 let check_program (dl,il) =
   check_instructions (get_declarations dl) il
                     
