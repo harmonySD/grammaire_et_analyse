3be22a7741b7488ab6f94df184668337022e6014
Clément Blérald <clement.blerald@gmail.com>
2021-04-28 12:14:08 +0200

    smooth

---
+++ code/ast.ml
@@ -0,0 +1,70 @@
+
+type expression = 
+  | Ident of string
+  | Plus of expression * expression
+  | Moins of expression * expression
+  | Mult of expression * expression
+  | Div of expression * expression
+  | Mun of int
+  | Nombre of int
+
+type instruction =
+  | Bloc of instruction list
+  | Avance of expression
+  | Tourne of expression
+  | Egal of string * expression
+  | BasPinceau
+  | HautPinceau
+  | Sias of expression * instruction * instruction
+  | Sialors of expression * instruction
+  | Tantq of expression * instruction
+  | Epaisseur of int
+  | Couleur of int * int * int
+
+
+type declaration = string
+
+type programme = declaration list * instruction list
+
+let rec as_string_decla l = 
+  match l with 
+    |[]->""
+    |x::y -> "Var "^x^" "^as_string_decla y
+
+let rec as_string = function
+  | Ident i -> i
+  | Nombre n -> string_of_int n
+  | Plus (l,r) -> as_string1 "+" l r
+  | Moins (l,r) -> as_string1 "-" l r
+  | Mult (l,r) -> as_string1 "*" l r
+  | Div (l,r) -> as_string1 "/" l r
+  | Mun n -> "(" ^ "-" ^ string_of_int n ^")"
+
+and as_string1 op l r =
+  "("^ as_string l ^ op ^ as_string r ^ ")"
+
+let rec as_string_instruction l = 
+  match l with 
+  |[] -> ""
+  |x::y-> as_string_instruction2 x ^as_string_instruction y
+
+and as_string_instruction2 = function
+  | BasPinceau -> "BasPinceau"
+  | HautPinceau -> "HautPinceau"
+  | Epaisseur t -> "(Epaisseur : "^string_of_int t ^")"
+  | Couleur (r,g,b) ->  "( " ^ "Couleur : " ^ "R : " ^ (string_of_int r) ^ "G : " 
+                        ^ (string_of_int g) ^ "B : " ^ (string_of_int b) ^ ")"
+  | Avance x -> "(" ^ "Avance " ^ as_string x ^")"
+  | Tourne x ->  "(" ^ "Tourne " ^ as_string x ^")"
+  | Egal (l,r) -> "(" ^ l ^ "=" ^ as_string r ^")"
+  | Sialors (e,i) -> "( Si"^(as_string e)^" Alors ["^(as_string_instruction2 i)^"] )"
+  | Sias (e,i,i2) -> "("^"Si "^(as_string e )^" Alors ["^(as_string_instruction2 i) ^"] Sinon ["^(as_string_instruction2 (i2))^"] )"
+  | Tantq (e,i) -> "("^"Tant que "^(as_string e)^" Faire ["^(as_string_instruction2 i)^"] )"
+  | Bloc ins -> (match ins with
+                      |[]-> ""
+                      |x::y-> as_string_instruction2 x ^as_string_instruction y)
+  
+(* Affiche l'arbre syntaxique du programme *)
+ let as_string_programme prog = 
+  let (decla,instruc) =prog in
+  "["^as_string_decla decla^ "; Debut" ^as_string_instruction instruc^" Fin]"
---
+++ code/typecheck.ml
@@ -0,0 +1,77 @@
+open Ast 
+
+exception Error of string
+
+let rec get_declarations = function 
+  | [] -> []
+  | v::r -> let rl = get_declarations r in 
+                if List.mem v rl
+                then raise (Error("Declarée deux fois : "^v))
+                else v::rl
+
+let rec type_expression decs = function
+  | Nombre _ | Mun _-> 1
+  | Ident s -> 
+    begin
+      try let _  = List.mem s decs in 1
+      with Not_found -> 0
+    end
+  | Plus (e1,e2) | Moins(e1,e2) | Mult(e1,e2) | Div(e1,e2)-> 
+    if (type_expression decs e1) = 1 && (type_expression decs e2) = 1
+    then 1
+    else 0 
+
+
+let rec check_instruction decs = function
+  | Egal (s,e) ->
+    (
+    try 
+      if List.mem s decs && (type_expression decs e) = 1
+      then ()
+      else raise (Error ("Pas declarée "^s))
+    with Not_found ->
+      raise (Error ("Pas declarée "^s))
+    )
+  | Avance e -> 
+    if (type_expression decs e) = 1 
+    then ()
+    else raise (Error ("Avance : argument n'est pas une expression reconnue")) 
+  | Tourne e ->
+    if (type_expression decs e) = 1 
+    then ()
+    else raise (Error ("Tourne : argument n'est pas une expression reconnue"))
+  | BasPinceau -> ()
+  | HautPinceau -> ()
+  | Sias(e,i1,i2) ->
+    if (type_expression decs e) = 1
+    then
+      let _ = check_instruction decs i1 
+      and _ = check_instruction decs i2 in
+      ()
+    else
+      raise (Error("e n'est pas une condition booléenne(sialorssinon)")) 
+  | Tantq(e,i) -> if (type_expression decs e) = 1  
+    then check_instruction decs i
+    else raise (Error("e n'est pas une condition booléenne (tantque)"))
+  | Sialors (e,i) -> 
+    if (type_expression decs e) = 1
+    then 
+      let _ = check_instruction decs i in ()
+    else 
+      raise (Error("e n'est pas une condition booléenne(sialors)"))
+  | Bloc b -> check_instructions decs b
+  | Couleur (r,g,b) -> 
+    if r >= 256 || r < 0 || g >= 256 && g < 0 || b >= 256 || b < 0 
+    then raise (Error("Erreur de type pour les couleurs"))
+    else ()
+  | Epaisseur t -> if t < 0 then raise(Error("Erreur de type pour l'épaisseur"))
+              
+and check_instructions decs il =
+  List.iter
+    (function i -> check_instruction decs i)
+    il
+
+(* Verification des types d'un programme complet *)
+let check_program (dl,il) =
+  check_instructions (get_declarations dl) il
+                    
